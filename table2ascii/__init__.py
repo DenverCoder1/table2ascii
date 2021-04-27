0d03c87e4b791567151fc0fc467c0a2d09b60592
Jonah Lawrence <jonah@freshidea.com>
2021-04-27 06:19:55 +0300

    Refactoring body and footer

--- table2ascii/__init__.py
+++ table2ascii/__init__.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Union
 from math import floor, ceil
 
 # constants
@@ -14,26 +14,17 @@ class TableToAscii:
         self.__header_row = header_row
         self.__body = body
         self.__footer_row = footer_row
-        self.__cell_width = 5  # ! TODO: Remove
         self.__cell_widths = [5, 5, 5, 5, 5]  # TODO: make this automatic
-        """
-        ╔═════╦═══════════════════════╗
-        ║  #  ║  G     H     R     S  ║
-        ╟─────╫───────────────────────╢
-        ║  1  ║  30    40    35    30 ║
-        ║  2  ║  30    40    35    30 ║
-        ╟─────╫───────────────────────╢
-        ║ SUM ║ 130   140   135   130 ║
-        ╚═════╩═══════════════════════╝
 
-        0111112111113111113111113111114
-        5     6     7     7     7     5
-        899999a99999b99999b99999b99999c
-        5     6     7     7     7     5
-        5     6     7     7     7     5
-        deeeeefeeeeegeeeeegeeeeegeeeeeh
-        5     6     7     7     7     5
-        i11111j11111k11111k11111k11111l
+        """
+        ╔═════╦═══════════════════════╗   0111112111113111113111113111114
+        ║  #  ║  G     H     R     S  ║   5     6     7     7     7     5
+        ╟─────╫───────────────────────╢   899999a99999b99999b99999b99999c
+        ║  1  ║  30    40    35    30 ║   5     6     7     7     7     5
+        ║  2  ║  30    40    35    30 ║   5     6     7     7     7     5
+        ╟─────╫───────────────────────╢   deeeeefeeeeegeeeeegeeeeegeeeeeh
+        ║ SUM ║ 130   140   135   130 ║   5     6     7     7     7     5
+        ╚═════╩═══════════════════════╝   i11111j11111k11111k11111k11111l
         """
         self.parts = {
             "top_left_corner": "╔",  # 0
@@ -60,136 +51,138 @@ class TableToAscii:
         }
 
     def __pad(self, text: str, width: int, alignment: int = ALIGN_CENTER):
+        """Pad a string of text to a given width with specified alignment"""
         if alignment == ALIGN_LEFT:
             return f" {text} " + (" " * (width - len(text) - 2))
         if alignment == ALIGN_CENTER:
-            before = " " * floor((width - len(text) - 2) / 2)
-            after = " " * ceil((width - len(text) - 2) / 2)
+            before = " " * ceil((width - len(text) - 2) / 2)
+            after = " " * floor((width - len(text) - 2) / 2)
             return before + f" {text} " + after
         if alignment == ALIGN_RIGHT:
             return (" " * (width - len(text) - 2)) + f" {text} "
         raise ValueError(f"The value '{alignment}' is not valid for alignment.")
 
+    def __row_to_ascii(
+        self,
+        left: str,
+        first_col_sep: str,
+        col_sep: str,
+        right: str,
+        filler: Union[str, List],
+    ) -> str:
+        """Assembles a row of the ascii table"""
+        # left edge of the row
+        output: str = left
+        # content across the first column
+        output += (
+            # edge or row separator if filler is specified
+            filler * self.__cell_widths[0]
+            if isinstance(filler, str)
+            # otherwise, first column content
+            else self.__pad(str(filler[0]), self.__cell_widths[0])
+        )
+        # separation of first column from the rest of the table
+        output += first_col_sep
+        # add remaining columns
+        for i in range(1, len(self.__header_row)):
+            # content between separators
+            output += (
+                # edge or row separator if filler is specified
+                filler * self.__cell_widths[i]
+                if isinstance(filler, str)
+                # otherwise, column content
+                else self.__pad(str(filler[i]), self.__cell_widths[i])
+            )
+            # add a separator
+            output += col_sep
+        # replace last seperator with symbol for edge of the row
+        output = output[0:-1] + right
+        return output + "\n"
+
     def __top_edge_to_ascii(self) -> str:
         """Assembles the top edge of the ascii table"""
-        # top-left corner of the table
-        output: str = self.parts["top_left_corner"]
-        # top edge above the first column
-        output += self.parts["top_and_bottom_edge"] * self.__cell_widths[0]
-        # tee separating first column from the rest of the table
-        output += self.parts["first_col_top_tee"]
-        # add remaining columns
-        for i in range(len(self.__header_row) - 1):
-            output += self.parts["top_and_bottom_edge"] * self.__cell_widths[i]
-            output += self.parts["top_tee"]
-        # replace last top tee with top-right corner
-        output = output[0:-1] + self.parts["top_right_corner"]
-        return output
+        return self.__row_to_ascii(
+            left=self.parts["top_left_corner"],
+            first_col_sep=self.parts["first_col_top_tee"],
+            col_sep=self.parts["top_tee"],
+            right=self.parts["top_right_corner"],
+            filler=self.parts["top_and_bottom_edge"],
+        )
+
+    def __bottom_edge_to_ascii(self) -> str:
+        """Assembles the top edge of the ascii table"""
+        return self.__row_to_ascii(
+            left=self.parts["bottom_left_corner"],
+            first_col_sep=self.parts["first_col_bottom_tee"],
+            col_sep=self.parts["bottom_tee"],
+            right=self.parts["bottom_right_corner"],
+            filler=self.parts["top_and_bottom_edge"],
+        )
 
     def __header_row_to_ascii(self) -> str:
         """Assembles the header row line of the ascii table"""
-        # add left edge of table
-        output: str = self.parts["left_and_right_edge"]
-        # add first column contents and padding
-        output += self.__pad(str(self.__header_row[0]), self.__cell_widths[0])
-        # separate first column from the rest of the table
-        output += self.parts["first_col_sep"]
-        # add the rest of the headers
-        output += self.parts["middle_edge"].join(
-            self.__pad(str(val), self.__cell_widths[i + 1])
-            for i, val in enumerate(self.__header_row[1:])
+        return self.__row_to_ascii(
+            left=self.parts["left_and_right_edge"],
+            first_col_sep=self.parts["first_col_sep"],
+            col_sep=self.parts["middle_edge"],
+            right=self.parts["left_and_right_edge"],
+            filler=self.__header_row,
         )
-        # add right edge of the table
-        output += self.parts["left_and_right_edge"]
-        return output
+
+    def __footer_row_to_ascii(self) -> str:
+        """Assembles the header row line of the ascii table"""
+        return self.__row_to_ascii(
+            left=self.parts["left_and_right_edge"],
+            first_col_sep=self.parts["first_col_sep"],
+            col_sep=self.parts["middle_edge"],
+            right=self.parts["left_and_right_edge"],
+            filler=self.__footer_row,
+        )
 
     def __header_sep_to_ascii(self) -> str:
         """Assembles the seperator below the header of the ascii table"""
-        # add left edge tee
-        output: str = self.parts["header_left_tee"]
-        # top edge above the first column
-        output += self.parts["header_row_sep"] * self.__cell_widths[0]
-        # tee separating first column from the rest of the table
-        output += self.parts["first_col_header_cross"]
-        # add remaining columns
-        for i in range(len(self.__header_row) - 1):
-            output += self.parts["header_row_sep"] * self.__cell_widths[i]
-            output += self.parts["header_row_cross"]
-        # replace last top tee with top-right corner
-        output = output[0:-1] + self.parts["right_tee"]
+        return self.__row_to_ascii(
+            left=self.parts["header_left_tee"],
+            first_col_sep=self.parts["first_col_header_cross"],
+            col_sep=self.parts["header_row_cross"],
+            right=self.parts["right_tee"],
+            filler=self.parts["header_row_sep"],
+        )
+
+    def __footer_sep_to_ascii(self) -> str:
+        """Assembles the seperator below the header of the ascii table"""
+        return self.__row_to_ascii(
+            left=self.parts["footer_left_tee"],
+            first_col_sep=self.parts["first_col_footer_cross"],
+            col_sep=self.parts["footer_row_cross"],
+            right=self.parts["right_tee"],
+            filler=self.parts["header_row_sep"],
+        )
+
+    def __body_to_ascii(self) -> str:
+        output: str = ""
+        for row in self.__body:
+            output += self.__row_to_ascii(
+                left=self.parts["left_and_right_edge"],
+                first_col_sep=self.parts["first_col_sep"],
+                col_sep=self.parts["middle_edge"],
+                right=self.parts["left_and_right_edge"],
+                filler=row,
+            )
         return output
 
     def to_ascii(self) -> str:
-        cols = len(self.__header_row)
-        # create table header
-        table = [
-            self.__top_edge_to_ascii(),
-            self.__header_row_to_ascii(),
-            self.__header_sep_to_ascii(),
-        ]
+        # add table header
+        table: str = self.__top_edge_to_ascii()
+        table += self.__header_row_to_ascii()
+        table += self.__header_sep_to_ascii()
         # add table body
-        for p in self.__body:
-            # add table row
-            table += [
-                # ║
-                self.parts["left_and_right_edge"]
-                +
-                #  1  ║
-                f"  {p[0]}  "
-                + self.parts["first_col_sep"]
-                #  40    40    40    40
-                + self.parts["middle_edge"].join(
-                    f"{p[i].rjust(4)} " for i in range(1, cols)
-                )
-                # ║
-                + self.parts["left_and_right_edge"]
-            ]
-        # footer row
-        table += [
-            # ╟
-            self.parts["footer_left_tee"]
-            # ─────╫
-            + (
-                self.parts["footer_row_sep"] * self.__cell_width
-                + self.parts["first_col_footer_cross"]
-            )
-            # ───────────────────────
-            + (
-                (
-                    self.parts["footer_row_sep"] * self.__cell_width
-                    + self.parts["footer_row_cross"]
-                )
-                * (cols - 1)
-            )[0:-1]
-            # ╢
-            + self.parts["right_tee"],
-            # ║
-            self.parts["left_and_right_edge"]
-            #  SUM ║
-            + f"{self.__footer_row[0].rjust(4)} " + self.parts["first_col_sep"]
-            #  120 ║ 120 ║ 120 ║ 120 ║
-            + self.parts["middle_edge"].join(
-                f"{self.__footer_row[i].rjust(4)} " for i in range(1, cols)
-            )
-            # ║
-            + self.parts["left_and_right_edge"],
-            # ╚
-            self.parts["bottom_left_corner"]
-            # ═════╩
-            + self.parts["top_and_bottom_edge"] * self.__cell_width
-            + self.parts["first_col_bottom_tee"]
-            # ════════════════════════
-            + (
-                (
-                    self.parts["top_and_bottom_edge"] * self.__cell_width
-                    + self.parts["bottom_tee"]
-                )
-                * (cols - 1)
-            )[0:-1]
-            # ╗
-            + self.parts["bottom_right_corner"],
-        ]
-        return "\n".join(table)
+        table += self.__body_to_ascii()
+        # add table footer
+        table += self.__footer_sep_to_ascii()
+        table += self.__footer_row_to_ascii()
+        table += self.__bottom_edge_to_ascii()
+        return table
 
 
 def table2ascii(header_row: List, body: List[List], footer_row: List) -> str:
