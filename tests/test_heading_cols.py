ca412800b3d4091d25d983affbcf6e738e46573f
Jonah Lawrence <jonah@freshidea.com>
2021-04-27 16:20:30 +0300

    Add column tests

--- tests/test_heading_cols.py
+++ tests/test_heading_cols.py
@@ -24,6 +24,20 @@ def test_first_column_heading():
     assert text == expected
 
 
+def test_first_column_heading_body_only():
+    text = t2a(
+        body=[["1", "30", "40", "35", "30"], ["2", "30", "40", "35", "30"]],
+        first_col_heading=True,
+    )
+    expected = (
+        "╔═══╦═══════════════════╗\n"
+        "║ 1 ║ 30   40   35   30 ║\n"
+        "║ 2 ║ 30   40   35   30 ║\n"
+        "╚═══╩═══════════════════╝\n"
+    )
+    assert text == expected
+
+
 def test_last_column_heading():
     text = t2a(
         header=["#", "G", "H", "R", "S"],
@@ -41,6 +55,20 @@ def test_last_column_heading():
         "╟───────────────────────╫─────╢\n"
         "║ SUM   130   140   135 ║ 130 ║\n"
         "╚═══════════════════════╩═════╝\n"
+    )
+    assert text == expected
+
+
+def test_last_column_heading_body_only():
+    text = t2a(
+        body=[["1", "30", "40", "35", "30"], ["2", "30", "40", "35", "30"]],
+        last_col_heading=True,
+    )
+    expected = (
+        "╔══════════════════╦════╗\n"
+        "║ 1   30   40   35 ║ 30 ║\n"
+        "║ 2   30   40   35 ║ 30 ║\n"
+        "╚══════════════════╩════╝\n"
     )
     assert text == expected
 
