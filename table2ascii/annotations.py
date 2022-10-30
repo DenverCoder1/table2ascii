b4ab51dd589c2914a7e669194ebd2061b74fbdd2
Jonah Lawrence <jonah@freshidea.com>
2022-10-30 16:27:46 -0600

    refactor: Support for mypy and add to workflow

--- table2ascii/annotations.py
+++ table2ascii/annotations.py
@@ -1,11 +1,10 @@
+import sys
 from abc import abstractmethod
 from typing import TYPE_CHECKING
 
-try:
-    # Python 3.8+
+if sys.version_info >= (3, 8):
     from typing import Protocol, runtime_checkable
-except ImportError:
-    # Python 3.7
+else:
     from typing_extensions import Protocol, runtime_checkable
 
 if TYPE_CHECKING:
@@ -15,8 +14,6 @@ if TYPE_CHECKING:
 @runtime_checkable
 class SupportsStr(Protocol):
     """An ABC with one abstract method __str__."""
-
-    __slots__ = ()
 
     @abstractmethod
     def __str__(self) -> str:
