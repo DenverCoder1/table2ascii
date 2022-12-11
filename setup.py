1affa28b447ff44e014d8e83f903ffcb2db9f2df
Jonah Lawrence <jonah@freshidea.com>
2022-12-11 00:15:34 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ def requirements():
 
 extras_require = {
     "docs": [
+        "sphinx==5.2.3",
         "enum-tools==0.9.0.post1",
         "sphinx-autobuild==2021.3.14",
         "sphinx-autodoc-typehints==1.11.1",
