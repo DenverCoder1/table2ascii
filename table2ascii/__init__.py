6e3fdb6aefb0aaa3759b588c8ecd67151901c540
Jonah Lawrence <jonah@freshidea.com>
2022-12-14 15:54:25 -0700

    chore: Bump version to 1.0.1

--- table2ascii/__init__.py
+++ table2ascii/__init__.py
@@ -8,7 +8,7 @@ from .preset_style import PresetStyle
 from .table_style import TableStyle
 from .table_to_ascii import table2ascii
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 __all__ = [
     "Alignment",
