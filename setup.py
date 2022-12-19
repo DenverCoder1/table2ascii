e8c76bb03d3d76a55cc5761fe7ca2370bfa65cc6
Jonah Lawrence <jonah@freshidea.com>
2022-12-18 17:05:01 -0700

    Set encoding type in setup

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 def version():
     version = ""
-    with open("table2ascii/__init__.py") as f:
+    with open("table2ascii/__init__.py", encoding="utf-8") as f:
         version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE)
     if not version:
         raise RuntimeError("version is not set")
@@ -18,7 +18,7 @@ def long_description():
     # check if README.md exists
     if not os.path.exists("README.md"):
         return ""
-    with open("README.md", "r") as fh:
+    with open("README.md", "r", encoding="utf-8") as fh:
         return fh.read()
 
 
@@ -26,7 +26,7 @@ def requirements():
     # check if requirements.txt exists
     if not os.path.exists("requirements.txt"):
         return []
-    with open("requirements.txt") as f:
+    with open("requirements.txt", encoding="utf-8") as f:
         return f.read().splitlines()
 
 
