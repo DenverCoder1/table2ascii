c8b159bb7e7e39d91a60b56989fcab5b4780fdc5
Jonah Lawrence <jonah@freshidea.com>
2023-06-25 11:32:16 -0600

    fix: Add name and version to setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,21 @@
 # /usr/bin/env python
+import re
+
 from setuptools import setup
 
+
+def version():
+    version = ""
+    with open("pyproject.toml") as f:
+        version = re.search(r'^version = ["\']([^"\']*)["\']', f.read(), re.M)
+    if not version:
+        raise RuntimeError("version is not set")
+    return version.group(1)
+
+
 setup(
+    name="table2ascii",
+    version=version(),
     packages=["table2ascii"],
     package_data={"table2ascii": ["py.typed"]},
 )
